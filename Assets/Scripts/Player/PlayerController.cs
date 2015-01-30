a135136b9be26c5b7d8a1a1d0b658c487cc2af35
brockuniera <craigbrockunier@wireless-10-148-44-51.public.utexas.edu>
2015-01-30 15:00:53 -0600

    Added local files to repo
    basically the initial commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//for List<T>
+using System.Collections.Generic;
+
+//probably make an abstract class or something
+//like an interface?
+[RequireComponent (typeof (PlayerInputHandler))]
+
+//children are controlled units
+public class PlayerController : MonoBehaviour {
+
+	//GOs I can control
+	private List<Transform> units;
+	private Transform currentUnit;
+
+	//input handler
+	private PlayerInputHandler input;
+
+	void Start(){
+		units = new List<Transform>();
+		units.Capacity = 3; //TODO
+		foreach(Transform child in transform){
+			units.Add(child);
+		}
+
+		input = GetComponent<PlayerInputHandler>();
+	}
+	
+	void FixedUpdate(){
+		//
+		//Process and distribute input
+		//
+
+		//change unit then ignore other input
+		if(input.bButton){
+			ChangeControlledUnit();
+			return;
+		}
+
+		//tell current thing to attack
+		if(input.aButton){
+			//units.
+		}
+
+		//tell current thing to move
+
+
+	}
+
+	private void ChangeControlledUnit(){
+
+	}
+}
---
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//this abstracts unity's input
+// needs to work for 2 players
+// include stick support
+public class PlayerInputHandler : MonoBehaviour {
+
+	//Properties
+	public int xAxis{
+		get{ return x; }
+	}
+	public int yAxis{
+		get{ return y; }
+	}
+	public bool aButton{
+		get{ return a; }
+	}
+	public bool bButton{
+		get{ return b; }
+	}
+
+	//current binds
+	private KeyBindings binds;
+
+	//key abstraction with defaults
+	private class KeyBindings{
+		//movement
+		public KeyCode up    = KeyCode.W;
+		public KeyCode down  = KeyCode.S;
+		public KeyCode left  = KeyCode.A;
+		public KeyCode right = KeyCode.D;
+
+		//actions
+		public KeyCode a = KeyCode.J;
+		public KeyCode b = KeyCode.K;
+	}
+
+	private int x, y;
+	private bool a, b;
+
+
+	//read input
+	void Update(){
+		if(Input.GetKey(binds.right))
+			x = 1;
+		else if(Input.GetKey(binds.left))
+			x = -1;
+		else
+			x = 0;
+
+		if(Input.GetKey(binds.up))
+			y = 1;
+		else if(Input.GetKey(binds.down))
+			y = -1;
+		else
+			y = 0;
+
+		a = Input.GetKey(binds.a);
+		b = Input.GetKey(binds.b);
+	}
+
+	public void SetDefaultBinds(){
+		//is this really how I should do this?
+		binds = new KeyBindings();
+	}
+
+}
