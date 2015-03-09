897eaf76f1a4711f4e178329385bc3e1f4f60620
brockuniera <craigbrockunier@wireless-10-148-23-186.public.utexas.edu>
2015-03-09 04:17:45 -0500

    Add code, no compile

---
+++ Assets/Scripts/Logic/PlayerSpawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Handles the logic involved in giving players units
+//and other initializations
+public class PlayerSpawner : MonoBehaviour {
+
+
+	//
+	//Private data
+	//
+
+	//References to Players 1 and 2
+	private GameObject _Player1, _Player2;
+
+
+	//
+	//Unity callbacks
+	//
+
+	void Start(){
+		//Get references to Players 1 and 2
+		if(!(_Player1 = GameObject.Find("Player 1")) || !(_Player2 = GameObject.Find("Player 2"))){
+			Debug.LogError("Couldn't find Players in Hierchay");
+			Application.Quit();
+		}
+	}
+	
+
+	//Adds Units to players
+	public void SpawnPlayerUnits(){
+		//this method does nothing right now...
+		var chooser = _Player1.GetComponent<ActiveChooser>();
+	}
+
+
+}
+
