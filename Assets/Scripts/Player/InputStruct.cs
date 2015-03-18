02a3fe0f5e54935b50d9e0176808248d1bb23f56
brockuniera <craigbrockunier@coolmac.local>
2015-03-18 09:16:10 -0500

    Implement timer in soldiers, clean code

--- Assets/Scripts/Player/InputStruct.cs
+++ Assets/Scripts/Player/InputStruct.cs
@@ -2,4 +2,9 @@ public struct InputStruct{
 	public sbyte x; //-1,0,1
 	public sbyte y; //-1,0,1
 	public bool a;  //t,f
+
+	public bool IsEmpty{
+		get{ return x == 0 && y == 0 && a == false; }
+	}
 }
+
